49e1fe33ea78561b1159395cfff4b321ce546e24
aakash25 <114928080+aakashmatkar25@users.noreply.github.com>
2023-10-14 23:47:19 +0530

    Add files via upload

---
+++ admin/pages/connect.php
@@ -0,0 +1,26 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Database connection
+    $conn = new mysqli('localhost:3307', 'root', '', 'wanderlust');
+    if($conn->connect_error){
+        die("Connection Failed: " . $conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO admin (name, email, password) VALUES (?, ?, ?)");
+        if (!$stmt) {
+            die("Error in statement preparation: " . $conn->error);
+        }
+        $stmt->bind_param("sss", $name, $email, $password);
+        $execval = $stmt->execute();
+        if (!$execval) {
+            die("Error in statement execution: " . $stmt->error);
+        }
+        header("Location: dashboard.html");
+        exit(); // Make sure to exit after a header redirect
+
+        $stmt->close();
+        $conn->close();
+    }
+?>
---
+++ admin/pages/sign-in.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$conn = new mysqli('localhost:3307', 'root', '', 'wanderlust');
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+
+$stmt = $conn->prepare("SELECT * FROM admin WHERE email=? AND password=?");
+if (!$stmt) {
+    die("Error in statement preparation: " . $conn->error);
+}
+
+$stmt->bind_param("ss", $email, $password);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows == 1) {
+    // Valid credentials, redirect to dashboard.html
+    header("Location: dashboard.html");
+    exit();
+} else {
+    // Invalid credentials, redirect back to login page
+    header("Location: sign-in.html"); // Assuming your login page is named login.html
+    exit();
+}
+
+$stmt->close();
+$conn->close();
+?>
